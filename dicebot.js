086d1b84a65e652dad334c5b5541395c043a4807
1linx <wearethedaddies@hotmail.com>
2016-02-05 14:06:05 +0000

    tidying up random code

--- dicebot.js
+++ dicebot.js
@@ -1,4 +1,68 @@
+var randomOrgApiData = [];
 var request = require('request');
+
+  // random.org API roller begins here.
+/**
+ * HOW TO Make an HTTP Call - POST
+ */
+ var https = require('https');
+// do a POST request
+// create the JSON object
+jsonObject = JSON.stringify({
+    "jsonrpc": "2.0",
+    "method": "generateIntegers",
+    "params": {
+        "apiKey": "deb0a73d-9211-4a7c-a194-7633d23d5193",
+        "n": 1,
+        "min": 1,
+        "max": 6,
+        "replacement": true,
+        "base": 10
+    },
+    "id": 4361
+    } );
+ 
+// prepare the header
+var postheaders = {
+    'Content-Type' : 'application/json',
+    'Content-Length' : Buffer.byteLength(jsonObject, 'utf8')
+};
+ 
+// the post options
+var optionspost = {
+    host : 'api.random.org',
+    port : 443,
+    path : '/json-rpc/1/invoke',
+    method : 'POST',
+    headers : postheaders
+};
+ 
+console.info('Options prepared:');
+console.info(optionspost);
+console.info('Do the POST call');
+ var contentsReturned = []
+// do the POST call
+var reqPost = https.request(optionspost, function(res) {
+    console.log("statusCode: ", res.statusCode);
+    // uncomment it for header details
+//  console.log("headers: ", res.headers);
+ 
+    res.on('data', function(d) {
+        console.info('POST result:\n');
+        process.stdout.write(d);
+        var contentsReturned = JSON.parse(d);
+        console.info('\n\nPOST completed');
+    });
+});
+ 
+// write the json data
+reqPost.write(jsonObject);
+reqPost.end();
+reqPost.on('error', function(e) {
+    console.error(e);
+});
+// var contentsReturned = JSON.parse(reqPost);
+console.log("\n\nhow about this: " + contentsReturned);
 
 module.exports = function (req, res, next) {
   // default roll is 2d6
@@ -38,6 +102,8 @@ module.exports = function (req, res, next) {
     rolls.push(currentRoll);
     total += currentRoll;
   }
+
+
   qualifiedTotal = parseInt(total);
   //if roll was senbt with plus sign, add it to total, else subtract it.
   if (matches[7] == "+") {
