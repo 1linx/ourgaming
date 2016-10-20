200aec7a7ea22ad0f7e7d71c0aed84c287bef5d4
1linx <wearethedaddies@hotmail.com>
2016-10-20 07:39:54 +0100

    added images, view engine, move index.

--- app.js
+++ app.js
@@ -3,16 +3,28 @@ var dicebot = require('./dicebot');
 var express = require('express');
 var bodyParser = require('body-parser');
 
-var app = express();
+var app = require('express')();
 var port = process.env.PORT || 3000;
+
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
 
 // body parser middleware
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-// test route
+// original roll, deprecated
+// app.get('/', function (req, res) {
+//     res.sendFile(__dirname + '/index.html');
+// });
+
+// testing views
+var slackLink = "https://hooks.slack.com/services/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";
 app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/index.html');
+    res.render('roller', {
+        slackLink: slackLink
+    });
 });
 
 // route that listens for a POST to /hello
@@ -23,7 +35,7 @@ app.post('/roll', dicebot);
 
 //static route for images
 // app.use(express.static('./imgs'));
-app.use('/static', express.static('./imgs'));
+app.use('/static', express.static('./public'));
 
 
 // error handler
@@ -35,4 +47,5 @@ app.use(function (err, req, res, next) {
 
 app.listen(port, function () {
   console.log('Slack bot listening on port ' + port);
+  
 });
